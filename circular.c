f79fd804163106bc40ac0bb7f668b7c7a86180e9
Trishalatae <53754142+Trishalatae@users.noreply.github.com>
2019-09-16 15:58:24 +0530

    Add files via upload

--- circular.c
+++ circular.c
@@ -1,236 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define qsize 5
-void enqueue(int q[] , int *r , int *count);
-void dequeue(int q[] , int *f , int *count);
-void display(int q[] , int *r , int *f , int *count);
-void main()
+#define size 5
+void enqueue(int q[],int *r,int *count)
 {
-  int q[qsize] , ch , r = -1 , f = 0 , count = 0;
-  while(1)
-  {
-    printf("\n\nEnter your choice\n");
-    printf("1. Insert\n2. Delete\n3. Display\n4. Exit\n\n");
-    scanf("%d", &ch);
-    switch(ch)
-    {
-      case 1 : enqueue(q , &r , &count) ;
-               break;
-      case 2 : dequeue(q, &f , &count) ;
-               break;
-      case 3 : display(q , &r , &f , &count) ;
-               break;
-      case 4 : exit(0);      default : printf("Invalid choice\n\n");
-25
-    }
-26
-  }
-27
+int item;
+printf("enter item to be inserted");
+scanf("%d",&item);
+if(*count>=size)
+{printf("queue is full");
 }
-28
-  
-29
-void enqueue(int q[] , int *r , int *count)
-30
-  {
-31
-    int item;
-32
-    printf("Enter the item to be inserted\n");
-33
-    scanf("%d", &item);
-34
-    if(*count >= qsize)
-35
-    {
-36
-      printf("Queue is full \n");
-37
-    }
-38
-      ((*r)++) % qsize ;
-39
-      q[*r] = item;
-40
-      *count = *count + 1;
-41
-  }
-42
-  
-43
-void dequeue(int q[] , int *f , int *count)
-44
-  {
-45
-    if(*count == 0)
-46
-    {
-47
-      printf("Queue is empty\n");
-48
-    }
-49
-    printf("Element deleted is %d", q[*f]);
-50
-    ((*f)++) % qsize ;
-51
-    *count = *count - 1;
-52
-  }
-53
-  
-54
-void display(int q[] , int *f , int *count)
-55
-  {
-56
-    int i;
-57
-    if(*count == 0)
-58
-    {
-59
-      printf("Circular queue is empty\n");
-60
-    }
-61
-    for(i = 0; i< *count ; i++)
-62
-    {
-63
-      printf("%d", q[*f]);
-64
-      ((*f)++) % qsize ;
-65
-    }
-66
-  }
-67
-  
-      default : printf("Invalid choice\n\n");
-    }
-  }
+
+((*r)++)%size;
+q[*r]=item;
+*count=*count+1;
+
+
 }
-  
-void enqueue(int q[] , int *r , int *count)
-  {
-    int item;
-    printf("Enter the item to be inserted\n");
-    scanf("%d", &item);
-    if(*count >= qsize)
-    {
-      printf("Queue is full \n");
-    }
-      ((*r)++) % qsize ;
-      q[*r] = item;      default : printf("Invalid choice\n\n");
-25
-    }
-26
-  }
-27
+void dequeue(int q[],int *f,int *count)
+{
+	if(*count==0)
+	printf("queue is empty");
+	
+	
+
+	printf("Element deleted is %d",q[*f]);
+	((*f)++)%size;
+	*count=*count-1;
+
+	
 }
-28
-  
-29
-void enqueue(int q[] , int *r , int *count)
-30
-  {
-31
-    int item;
-32
-    printf("Enter the item to be inserted\n");
-33
-    scanf("%d", &item);
-34
-    if(*count >= qsize)
-35
-    {
-36
-      printf("Queue is full \n");
-37
-    }
-38
-      ((*r)++) % qsize ;
-39
-      q[*r] = item;
-40
-      *count = *count + 1;
-41
-  }
-42
-  
-43
-void dequeue(int q[] , int *f , int *count)
-44
-  {
-45
-    if(*count == 0)
-46
-    {
-47
-      printf("Queue is empty\n");
-48
-    }
-49
-    printf("Element deleted is %d", q[*f]);
-50
-    ((*f)++) % qsize ;
-51
-    *count = *count - 1;
-52
-  }
-53
-  
-54
-void display(int q[] , int *f , int *count)
-55
-  {
-56
-    int i;
-57
-    if(*count == 0)
-58
-    {
-59
-      printf("Circular queue is empty\n");
-60
-    }
-61
-    for(i = 0; i< *count ; i++)
-62
-    {
-63
-      printf("%d", q[*f]);
-64
-      ((*f)++) % qsize ;
-65
-    }
-66
-  }
-67
-  
-      *count = *count + 1;
-  }
-  
-void dequeue(int q[] , int *f , int *count)
-  {
-    if(*count == 0)
-    {
-      printf("Queue is empty\n");
-    }
-    printf("Element deleted is %d", q[*f]);
-    ((*f)++) % qsize ;
-    *count = *count - 1;
-  }
-  
-void display(int q[] , int *r ,int *f , int *count)
-  {
-    int i;
-    if(*count == 0)
-    {
-      printf("Circular queue is empty\n");
-    }
-   printf("\nElements in Circular Queue are: "); 
+void display(int q[],int *f,int *count,int *r)
+{
+	int i;
+	if(*count==0)
+	printf("circular queue is empty");
+	printf("\nElements in Circular Queue are: "); 
     if (*r >= *f) 
     { 
         for (int i = *f; i <= *r; i++) 
@@ -238,17 +42,36 @@ void display(int q[] , int *r ,int *f , int *count)
     } 
     else
     { 
-        for (int i = *f; i < qsize; i++) 
+        for (int i = *f; i < size; i++) 
             printf("%d ", q[i]); 
   
         for (int i = 0; i <= *r; i++) 
             printf("%d ", q[i]); 
     } 
-  }
-  
-    
-      
-    
-    
-      
-      
+}
+
+int main()
+{
+int q[size],r=-1,f=0,count=0,ch;
+while(1)
+{
+	printf("Enter your choice\n");
+	scanf("%d",&ch);
+	switch(ch)
+	{
+	case 1:enqueue(q,&r,&count);
+		break;
+	case 2:dequeue(q,&f,&count);
+		break;
+	case 3:display(q,&f,&count,&r);
+		break;
+	case 4:exit(1);
+		break;
+	default:printf("Invalid choice");
+		break;
+	}
+
+}
+return 0;
+}
+
